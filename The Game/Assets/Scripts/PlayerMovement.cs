394cc4a430b0bfc4ce84f0020ea0f6b954300495
Alexander Walker <22206619@bucks.ac.uk>
2023-04-23 16:25:33 +0100

    bodge collision detection

--- The Game/Assets/Scripts/PlayerMovement.cs
+++ The Game/Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,18 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     private float speed = 2.0f;
     Vector2 direction;
+    Vector2 dmov;
+    float dt;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent <Rigidbody2D> ();
     }
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.W)) {
             direction = Vector2.up;
@@ -35,8 +37,15 @@ public class PlayerMovement : MonoBehaviour
         }
 
         Vector2 position = rb.position;
-        Vector2 dmov = direction * speed * Time.deltaTime;
+        dt = Time.fixedDeltaTime;
+        dmov = direction * speed * dt;
         rb.position = (position + dmov);
     }
 
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "wall") {
+            Vector2 back = (direction * speed * -0.04f);
+            rb.position = (rb.position + back);
+        }
+    }
 }
