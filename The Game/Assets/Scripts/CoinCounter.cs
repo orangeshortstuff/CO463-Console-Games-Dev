a96a6a1479012c49bd0704c153590c74abc58fb3
Chi Ho Lau <22127935@bucks.ac.uk>
2023-05-04 11:45:42 +0100

    added coin counter system and score UI

---
+++ The Game/Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+    Text counterText;
+
+    // Start is called before first frame update
+    void Start()
+    {
+        counterText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The current number of coins to display
+        if (counterText.text != CoinCollection.totalCoins.ToString())
+        {
+            counterText.text = CoinCollection.totalCoins.ToString();
+        }
+    }
+}
