63cb5c9ac1ef0e40670e05e8a016161649c78417
Chi Ho Lau <22127935@bucks.ac.uk>
2023-05-04 11:01:47 +0100

    update coins

--- The Game/Assets/Scripts/CoinCollection.cs
+++ The Game/Assets/Scripts/CoinCollection.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class CoinCollection : MonoBehaviour
 {
-    //Keep track of total picked coins (Since the value is static, it can be accessed at "SC_2DCoin.totalCoins" from any script)
+    //Counts the total amount of coins picked up 
     public static int totalCoins = 0;
 
     void Awake()
     {
-        //Make Collider2D as trigger 
+        //Make Collider2D a trigger 
         GetComponent<Collider2D>().isTrigger = true;
     }
 
     void OnTriggerEnter2D(Collider2D c2d)
     {
-        //Destroy the coin if Object tagged Player comes in contact with it
+        //Destroys the coin if the Object tagged Player comes in contact with it
         if (c2d.CompareTag("Player"))
         {
             //Add coin to counter
             totalCoins++;
-            //Test: Print total number of coins
+            //Print total number of coins
             Debug.Log("You currently have " + CoinCollection.totalCoins + " Coins.");
             //Destroy coin
             Destroy(gameObject);
