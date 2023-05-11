fc9affb648aeb363dbc55b93e38e7c49421b0072
Chi Ho Lau <22127935@bucks.ac.uk>
2023-05-11 11:17:14 +0100

    main menu scene/player trail

---
+++ The Game/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MouseClick : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        if (this.name == "PlayButton")
+        {
+
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
+
