487a54f0fbfe7c3bca96c656af06dd81e1928c74
ChiHoLau888 <lauchiho7@gmail.com>
2023-05-21 22:58:30 +0100

    Updated Main Menu Script and added "Quit Game" Button

---
+++ The Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{   
+    //Opens Level 1 Scene
+    public void OpenLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    //Opens Level 2 Scene
+    public void OpenLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+    //Exits Application
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
