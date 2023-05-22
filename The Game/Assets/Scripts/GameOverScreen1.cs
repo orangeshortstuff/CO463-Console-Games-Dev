6d642a684fe0f1d70d9fb9712405aeb7db00ad05
ChiHoLau888 <lauchiho7@gmail.com>
2023-05-22 17:32:46 +0100

    Added Level 2 Game Over Screen

---
+++ The Game/Assets/Scripts/GameOverScreen1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen1 : MonoBehaviour
+{  
+    //Opens Level 1 Scene
+    public void OpenLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    //Opens Main Menu
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
--- The Game/Assets/Scripts/GameOverScreen2.cs
+++ The Game/Assets/Scripts/GameOverScreen2.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverScreen : MonoBehaviour
+public class GameOverScreen2 : MonoBehaviour
 {  
-    //Opens Level 1 Scene
-    public void OpenLevel1()
+    //Opens Level 2 Scene
+    public void OpenLevel2()
     {
-        SceneManager.LoadScene("Level 1");
+        SceneManager.LoadScene("Level 2");
     }
 
     //Opens Main Menu
